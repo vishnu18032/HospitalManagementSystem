d9458797ce9bc382b9f8e90e699ffb579e75ea1d
Vishnu <jadhavvishnu666@.com>
2024-03-06 13:06:28 +0530

    nothing

---
+++ src/HospitalManagementSystem/Doctor.java
@@ -0,0 +1,66 @@
+package HospitalManagementSystem;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+public class Doctor {
+
+    private Connection connection;
+
+    public Doctor(Connection connection){
+        this.connection =  connection;
+    }
+
+
+
+
+    public  void viewDoctor(){
+        String query = "SELECT * FROM doctors";
+
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            System.out.println("Doctors");
+            System.out.println("+--------------+------------------------+------------+----------+");
+            System.out.println(" | Doctor Id   | Name                   | Specialization        |");
+            System.out.println("+--------------+------------------------+------------+----------+");
+
+            while(resultSet.next()){
+                int id = resultSet.getInt("id");
+                String name = resultSet.getString("name");
+                String specialization = resultSet.getString("specialization");
+                System.out.printf("|%-13s|%-25s|%-23s|\n",id,name,specialization);
+                System.out.println("+--------------+------------------------+------------+----------+");
+
+            }
+
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
+
+    //check patient info
+
+    public boolean getDoctorsById(int id){
+        String query = "select * from doctors WHERE id = ?";
+
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setInt(1,id);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if(resultSet.next()){
+                return  true;
+            }else {
+                return false;
+            }
+
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+        return  false;
+    }
+
+}
